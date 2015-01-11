2228ae34f73d28de56f581db64f7508b6c7e0d33
Nelson Chen <crazysim@gmail.com>
2015-01-11 05:07:32 -0800

    Fixed up some bolding issues

--- oh-my-fish/themes/nelsonjchen/fish_prompt.fish
+++ oh-my-fish/themes/nelsonjchen/fish_prompt.fish
@@ -1,14 +1,10 @@
-function __return_code
-  echo "unimpted"
-end
-
 function fish_prompt
-	set user (set_color -o cyan)(whoami)(set_color normal)
+	set user (set_color --bold cyan)(whoami)(set_color normal)
 
-	set machine (set_color -o blue)(hostname| cut -d . -f 1)(set_color normal)
+	set machine (set_color --bold blue)(hostname| cut -d . -f 1)(set_color normal)
 
 	set -l realhome ~
-	set directory (set_color -o green)(echo $PWD | sed -e "s|^$realhome|~|")(set_color normal)
+	set directory (set_color --bold green)(echo $PWD | sed -e "s|^$realhome|~|")(set_color normal)
 
 	set -g __fish_git_prompt_show_informative_status 'yes'
 	set -g __fish_git_prompt_showcolorhints 'yes'
@@ -17,14 +13,22 @@ function fish_prompt
 	echo -s $user (set_color yellow)'@' $machine ':' $directory $git
 
 	if [ $USER = "root" ]
-	  set caret (set_color red ) "#"
+		set caret (set_color red ) "#"
 	else
-	  set caret (set_color magenta) "\$"
+		set caret (set_color magenta) "\$"
 	end
 
 	echo -s $caret ' '(set_color normal)
 end
 
 function fish_right_prompt -d "Write out the right prompt"
-	echo -ns (__return_code)" "(date "+%D")" - "(date "+%T")
+	set -l last_status $status
+
+	if test $last_status -ne 0
+		set_color --bold red
+		echo -ns $last_status ' :( '
+		set_color normal
+	end
+
+	echo -ns (date "+%D")" - "(date "+%T")
 end
