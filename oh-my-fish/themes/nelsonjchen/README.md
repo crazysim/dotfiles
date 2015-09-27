6f855c563ce81f41b5c0d105100f223e30f9a06b
Nelson Chen <crazysim@gmail.com>
2015-09-27 03:33:40 -0700

    Complete transition of init script to new OMF

--- oh-my-fish/themes/nelsonjchen/README.md
+++
@@ -1,3 +0,0 @@
-# Nelson Chen's Theme
-
-This theme is based off of re5et's theme in Oh-My-Zsh.
---
+++ omf/init.fish
@@ -0,0 +1,9 @@
+alias j=z
+
+rbenv init - | source
+pyenv init - | source
+
+set PATH $HOME/bin $PATH
+set PATH /usr/texbin $PATH
+
+fish_vi_mode
