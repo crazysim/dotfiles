4806da7757290bbdcbf1ca27efb53846ae4cab6c
Nelson Chen <crazysim@gmail.com>
2015-12-13 02:35:37 -0800

    Add multirust bin paths to fish init

--- omf/init.fish
+++ omf/init.fish
@@ -4,7 +4,9 @@ rbenv init - | source
 pyenv init - | source
 
 set PATH $HOME/bin $PATH
+set PATH $HOME/.multirust/toolchains/stable/cargo/bin $PATH
+set PATH $HOME/.multirust/toolchains/beta/cargo/bin $PATH
+set PATH $HOME/.multirust/toolchains/nightly/cargo/bin $PATH
 set PATH /usr/texbin $PATH
 
 fish_vi_mode
-
