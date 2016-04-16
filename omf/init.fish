f9584b933268859ebe1f911389f95432e4cfabe0
Nelson Chen <crazysim@gmail.com>
2016-04-16 13:50:58 -0700

    Switch from multirust to rustup.rs

--- omf/init.fish
+++ omf/init.fish
@@ -4,9 +4,7 @@ rbenv init - | source
 pyenv init - | source
 
 set PATH $HOME/bin $PATH
-set PATH $HOME/.multirust/toolchains/stable/cargo/bin $PATH
-set PATH $HOME/.multirust/toolchains/beta/cargo/bin $PATH
-set PATH $HOME/.multirust/toolchains/nightly/cargo/bin $PATH
+set PATH $HOME/.cargo/bin $PATH
 set PATH /usr/texbin $PATH
 
 fish_vi_mode
